f3c5e576867d3bfd72dfb4f01f5710e048baff6c
Sardor0047 <macbookpro@MacBook-Pro-MacBook.local>
2024-09-25 14:26:53 +0500

    qwerty

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ def get_prices(data):
         list: list of prices
     """
 
+    x = data.split('\n')
+    maxsulot = ''
+
+    i = 0
+    while i < len(x):
+
+        narx = x[i].split(',')
+        maxsulot += f"{i+1} {narx[2]} \n"
+        i += 1
+
+    return maxsulot
 
 def get_products(data):
     """
@@ -24,6 +35,17 @@ def get_products(data):
         list: list of products
     """
 
+    x = data.split('\n')
+    maxsulot = ''
+
+    i = 0
+    while i < len(x):
+
+        lst = x[i].split(',')
+        maxsulot += f"{i+1} {lst[0]} \n"
+        i +=1
+
+    return maxsulot
 def get_expensive(prices):
     """
     Finds the most expensive product of index
@@ -35,8 +57,35 @@ def get_expensive(prices):
         int: index of most expensive product
     """
 
+    x = data.split('\n')
+    maxsulot = []
+
+    i = 1
+    while i < len(x):
+
+        narx = x[i].split(',')
+        maxsulot.append((narx[2]))
+        i += 1
+
+    price = []
+
+    y = 0
+    while y < len(maxsulot):
+        
+        just = maxsulot[y]
+        price.append(float(just[1:]))
+        y += 1
+
+    top_price = (price[0])
+    n = 0
+    while n < len(price):
+        if top_price < price[n]:
+            top_price =(price[n])
+        n += 1
+    return f"${top_price}"
     
 
 # Read data from file
-f = open("data.csv")
+f = open("grocery_list_basic/data.csv")
 data = f.read()
+print(get_products(data))
